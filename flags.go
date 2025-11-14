e12a619b86cb0d32424ad5a4be7413aa15f1e540
Emmanuel Gautier <git@emmanuelgautier.fr>
2025-11-14 21:09:00 +0100

    feat: add missing basic and digest flags

--- flags.go
+++ flags.go
@@ -7,8 +7,10 @@ import (
 func RegisterFlags(flags *pflag.FlagSet) {
 	flags.BoolP("append", "a", false, "Append to target file(s) instead of overwriting")
 	flags.StringP("cookie", "b", "", "Send cookies from string/file")
+	flags.BoolP("basic", "", false, "Use HTTP Basic Auth (use with -u)")
 	flags.BoolP("compressed", "", false, "Request compressed response")
 	flags.StringP("data", "d", "", "HTTP POST data")
+	flags.BoolP("digest", "", false, "Use HTTP Digest Auth (use with -u)")
 	flags.BoolP("fail", "f", false, "Fail fast with no output on HTTP errors")
 	flags.StringP("form", "F", "", "Specify multipart MIME data")
 	flags.StringP("head", "I", "", "Show document info only")
@@ -25,8 +27,8 @@ func RegisterFlags(flags *pflag.FlagSet) {
 	flags.BoolP("silent", "s", false, "Silent mode")
 	flags.StringP("referer", "e", "", "Send Referer Page information.")
 	flags.StringP("upload-file", "T", "", "Transfer local FILE to destination")
-	flags.StringP("url", "u", "", "URL to work with")
+	flags.StringP("url", "", "", "URL to work with")
 	flags.StringP("user-agent", "A", "", "Send User-Agent <name> to server")
-	flags.StringP("user", "U", "", "Server user and password")
+	flags.StringP("user", "u", "", "Server user and password")
 	flags.BoolP("verbose", "v", false, "Make the operation more talkative")
 }
